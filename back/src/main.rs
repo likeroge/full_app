4097a2f150b220c1530a9237e8115baf28246aa5
likeroge <likeroge@gmail.com>
2026-03-09 18:44:03 +0400

    second

--- back/src/main.rs
+++ back/src/main.rs
@@ -5,19 +5,36 @@ mod models;
 
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
+use axum::{http::HeaderValue, routing::get, Router};
+use reqwest::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    Method,
+};
 use tokio::net::TcpListener;
 use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
-    let cors_layer = CorsLayer::permissive();
+    // let cors_layer = CorsLayer::permissive();
+
+    let cors = CorsLayer::new()
+        .allow_origin("*".parse::<HeaderValue>().expect("Cannot parse"))
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::PATCH,
+        ])
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+    // .allow_credentials(true);
 
     let router = Router::new()
         .route("/api", get(|| async { "Hello, World!" }))
         .route("/api/json", get(|| async { "{\"key\":\"value\"}" }))
         .route("/api/users", get(handlers::users::all_users))
-        .layer(cors_layer);
+        .layer(cors);
+    // .layer(cors_layer);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 5005));
     let tcp = TcpListener::bind(&addr)
