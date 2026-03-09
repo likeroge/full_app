43a8d9233045f62e245cc86fda457cf7ee26b824
likeroge <likeroge@gmail.com>
2026-03-09 11:47:07 +0400

    first commit

---
+++ back/src/main.rs
@@ -0,0 +1,16 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/api", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5005));
+    let tcp = TcpListener::bind(&addr)
+        .await
+        .expect("failed to bind to socket");
+    println!("Server started!");
+    axum::serve(tcp, router).await.expect("Error start server");
+}
