1f08e35de4c743bbc3d09091a72609f56df9bbf6
likeroge <likeroge@gmail.com>
2026-03-15 20:44:51 +0400

    upd docker file

--- back/src/main.rs
+++ back/src/main.rs
@@ -46,10 +46,10 @@ async fn main() {
         .route("/api/json", get(|| async { "{\"key\":\"value\"}" }))
         .route("/api/users", get(handlers::users::all_users))
         .route("/api/users", post(handlers::users::create_user))
-        .route("/api/users/{id}", get(handlers::users::get_by_id));
-    // .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-    // .layer(cors)
-    // .layer(Extension(pool));
+        .route("/api/users/{id}", get(handlers::users::get_by_id))
+        // .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .layer(cors)
+        .layer(Extension(pool));
     //
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 5005));
